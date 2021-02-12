fe0c6d5da2a08ac7bafc00a40c900ee1776acee8
Salvador Balkus <salbalkus@gmail.com>
2021-02-11 20:39:14 -0500

    Configured bot to pull scoreboard from an S3 bucket. Please ensure that you have the updated .env file if you wish to run the bot

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
 import discord
 import os
+import boto3
 import pandas as pd
 from dotenv import load_dotenv
 
 load_dotenv()
 
 client = discord.Client()
+s3 = boto3.resource(
+    service_name='s3',
+    region_name='us-east-2',
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+)
+
+obj = s3.Bucket('bdc-scoreboard').Object("scoreboard.csv").get()
+scoreboard = pd.read_csv(obj["Body"], index_col = 0)
+scoreboard.to_csv("scoreboard.csv")
 
 @client.event
 async def on_ready():
@@ -22,12 +33,14 @@ async def on_message(message):
     if message.author.name not in scoreboard.Member.values:
         scoreboard = scoreboard.append(pd.DataFrame({"Member" : [message.author.name], "Score" : [0]}), ignore_index = True, sort = True)
         scoreboard.to_csv("scoreboard.csv")
+        s3.Bucket('bdc-scoreboard').upload_file(Filename='scoreboard.csv', Key='scoreboard.csv')
         
     if message.content.startswith('/add'):
         name = " ".join(message.content.split(" ")[2:])
         value = int(message.content.split(" ")[1])
         scoreboard.at[scoreboard[scoreboard.Member == name].index[0], "Score"] += value
         scoreboard.to_csv("scoreboard.csv")
+        s3.Bucket('bdc-scoreboard').upload_file(Filename='scoreboard.csv', Key='scoreboard.csv')
     
     
     if message.content.startswith('/scoreboard'):
