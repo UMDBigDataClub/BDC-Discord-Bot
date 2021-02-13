58030bc4d98484b75ff771806563ca9191018013
Salvador Balkus <salbalkus@gmail.com>
2021-02-12 23:42:02 -0500

    made it so S3 is updated every time someone's score is changed, rather than when the bot disconnects

--- main.py
+++ main.py
@@ -24,15 +24,12 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-  
-@client.event
-async def on_disconnect():
-    global S3
-    s3.Bucket('bdc-scoreboard').upload_file(Filename='scoreboard.csv', Key='scoreboard.csv')
+    
 
 @client.event
 async def on_message(message):
     global scoreboard
+    global S3
     
     if message.author == client.user:
         return
@@ -44,6 +41,7 @@ async def on_message(message):
     if message.author.name not in scoreboard.Member.values:
         scoreboard = scoreboard.append(pd.DataFrame({"Member" : [message.author.name], "Score" : [0]}), ignore_index = True, sort = True)
         scoreboard.to_csv("scoreboard.csv")
+        s3.Bucket('bdc-scoreboard').upload_file(Filename='scoreboard.csv', Key='scoreboard.csv')
     
     # Adds and subtracts values to a person's score
     if message.content.startswith('/add'):
@@ -51,6 +49,7 @@ async def on_message(message):
         value = int(message.content.split(" ")[1])
         scoreboard.at[scoreboard[scoreboard.Member == name].index[0], "Score"] += value
         scoreboard.to_csv("scoreboard.csv")
+        s3.Bucket('bdc-scoreboard').upload_file(Filename='scoreboard.csv', Key='scoreboard.csv')
 
     
     # Prints the score of everyone in the scoreboard.csv file
@@ -95,7 +94,6 @@ async def on_reaction_add(reaction, user):
         #channel = reaction.message.channel
         await user.add_roles(member_role)
         
-
 
 client.run(os.getenv('TOKEN'))
 
