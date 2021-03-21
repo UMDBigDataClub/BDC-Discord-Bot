0b75407656a8d6bb1b5d6cf94cb9ebb6953207da
Salvador Balkus <salbalkus@gmail.com>
2021-03-21 14:08:56 -0400

    prototype update

--- src/prototype.py
+++ src/prototype.py
@@ -3,6 +3,7 @@ import discord
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
@@ -10,6 +11,8 @@ intents = discord.Intents.default()
 #intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+prev_message = ""
 
 @bot.event
 async def on_ready():
@@ -19,5 +22,28 @@ async def on_ready():
 async def honk(ctx):
     await ctx.send(":bird: *doot doot* :bird:")
 
-print(os.getenv('TOKEN'))
+@bot.event
+async def on_message(message):
+    global prev_message
+
+    if message.author == bot.user:
+        return
+
+    #When someone is tagged, it replaces the text with their user id formatted like so.
+    #DON'T FORGET TO REPLACE THIS USER ID TO THE REAL BOT WHEN DEPLOYED
+    #if "<@!823172527697297469>" in message.content:
+    #    r = requests.post(
+    #        "https://api.deepai.org/api/text-generator",
+    #        data={
+    #            'text': prev_message,
+    #        },
+    #        headers={'api-key': '5a969f50-dad3-4af3-8177-72e5a1ad8ed3'}
+    #    )
+    #    gpt2_output = r.json()['output']
+    #    gpt2_output = gpt2_output.split("\n\n")[1]
+    #    await message.channel.send(gpt2_output)
+
+    prev_message = message.content
+
+
 bot.run(os.getenv('TOKEN'))
